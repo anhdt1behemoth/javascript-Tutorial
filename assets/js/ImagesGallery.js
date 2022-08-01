f63259cbcae5ebe247180e8e95a5a8fbad60ffa6
anhdt1behemoth <anhdt@behemoth.vn>
2022-08-01 12:08:31 +0700

    search/layout

---
+++ assets/js/ImagesGallery.js
@@ -0,0 +1,51 @@
+
+let images = document.querySelectorAll('.images img')
+let prev = document.querySelector('.prev')
+let next = document.querySelector('.next')
+let close = document.querySelector('.close')
+let galleryImg = document.querySelector('.gallery__inner img')
+let gallery = document.querySelector('.gallery')
+
+let current = 0;
+
+function galleryShow() {
+    if (current == 0) {
+        prev.classList.add('hide')
+    }else{
+        prev.classList.remove('hide')
+    }
+    galleryImg.src = images[current].src
+    gallery.classList.add('show')
+}
+images.forEach((item,index) => {
+    item.addEventListener('click', function() {
+        current = index
+        galleryImg.src = images[current].src
+        gallery.classList.add('show')
+    })
+})
+
+close.addEventListener('click', function(){
+    gallery.classList.remove('show')
+})
+
+document.addEventListener('keydown', function(e){
+    if(e.keyCode == 27){
+        gallery.classList.remove('show')
+    }
+})
+
+prev.addEventListener('click', function(){
+    if(current > 0) {
+        current--
+        galleryShow()
+    }
+
+})
+
+next.addEventListener('click', function() {
+    if(current < images.length - 1){
+        current++
+        galleryShow()
+    }
+})
